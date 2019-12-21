7d83329ba850c568f0c62c1adb514551f6122cbd
RdPhnX <volodymyr.vedmedera@gmail.com>
2019-12-21 16:35:59 +0200

    Add project files.

---
+++ HomePayments/IPayment.cs
@@ -0,0 +1,9 @@
+﻿namespace HomePayments
+{
+    public interface IPayment
+    {
+        string Type { get; set; }
+
+        void SubmitReadings(int currentReadings);
+    }
+}
---
+++ HomePayments/Payment.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomePayments
+{
+    public class Payment : IPayment
+    {
+        private Dictionary<DateTime, int> readings;
+
+        public Payment(string type)
+        {
+            Type = type;
+            readings = new Dictionary<DateTime, int>();
+        }
+        
+        public string Type { get; set; }
+
+        public void SubmitReadings(int currentReadings)
+        {
+            readings.Add(DateTime.UtcNow, currentReadings);
+        }
+    }
+}
